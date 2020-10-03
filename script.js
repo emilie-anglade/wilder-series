3bb2887a34e3049341c805f36e2921e8612f7fe7
Alexandre-Dietsch <dietschalex@gmail.com>
2020-10-03 20:30:34 +0200

    burger-menu animation

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let button = document.querySelector(".nav-toggle");
+const button = document.querySelector(".nav-toggle");
+const borderRadius = document.querySelector(".navbar");
 
 button.addEventListener("click", function () {
   let list = document.querySelector(".dropdown-menu-content");
   list.classList.toggle("visible");
+  borderRadius.classList.toggle("open");
 });
-
-  
