2845189f0f78f7998bacf03fc5567f25214b8af8
Alexandre-Dietsch <dietschalex@gmail.com>
2020-10-03 21:33:27 +0200

    Feature Javascript button heart

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+//SELECTORS
 const button = document.querySelector(".nav-toggle");
 const borderRadius = document.querySelector(".navbar");
+const favoriteLogo = document.querySelector(".favorite-logo");
+const imageContainer = document.querySelector("link-page");
 
+//ADD EVENTS LISTENERS
 button.addEventListener("click", function () {
   let list = document.querySelector(".dropdown-menu-content");
   list.classList.toggle("visible");
   borderRadius.classList.toggle("open");
 });
+
+const favoriteButton = () => {
+  favoriteLogo.addEventListener("click", (event) => {
+    imageContainer.removeAttribute("href");
+    event.target.style.color = "red";
+  });
+};
